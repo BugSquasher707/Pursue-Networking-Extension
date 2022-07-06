13862a3eec43e9f46709b4509be730c926b045c0
M Arsl Thaheem <80126340+Thaheem001@users.noreply.github.com>
2022-07-06 15:48:58 +0500

    update today dashboard styling page

--- Public/Assets/js/script.js
+++ Public/Assets/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     //     $('#result').text("id : " + userid + ", name : " + username);
     // });
     $(document).mouseup(function (e) {
-        var container = $(".input-inner");
+        let container = $(".input-inner");
         if (!container.is(e.target) && container.has(e.target).length === 0) {
             $('#searchTopSelect').select2('close');
         }
@@ -21,5 +21,13 @@ $(document).ready(function () {
 
 
 
-
+    // hide modal on click outside dive 
+    // $(document).mouseup(function (e) {
+    //     let containerClick = $(".filter-item-1");
+    //     let containerHide = $(".date-filter-list");
+    //     if (!containerClick.is(e.target) && containerClick.has(e.target).length === 0) {
+    //         containerHide.css("max-height", "0px")
+    //         containerHide.css("visibility", "hidden")
+    //     }
+    // });
 });
